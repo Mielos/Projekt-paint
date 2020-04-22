9e2e50ed34857be86c0946f8fd0b4ef818abce50
Mielos <62388517+Mielos@users.noreply.github.com>
2020-04-22 21:45:18 +0200

    Add files via upload

---
+++ image.04/image/Form1.Designer.cs
@@ -0,0 +1,302 @@
+﻿namespace image
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.btn_addText = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button8 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.Ksztalty = new System.Windows.Forms.Button();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(252, 121);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(801, 474);
+            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.AutoSize;
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseMove += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseMove);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(43, 33);
+            this.button1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button1.Name = "button1";
+            this.button1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button1.Size = new System.Drawing.Size(152, 26);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Otworz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(43, 121);
+            this.button2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button2.Name = "button2";
+            this.button2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button2.Size = new System.Drawing.Size(152, 26);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Szary";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(43, 151);
+            this.button3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button3.Name = "button3";
+            this.button3.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button3.Size = new System.Drawing.Size(152, 26);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Sepia";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(200, 33);
+            this.button4.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.button4.Name = "button4";
+            this.button4.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.button4.Size = new System.Drawing.Size(152, 26);
+            this.button4.TabIndex = 4;
+            this.button4.Text = "Zapisz";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // btn_addText
+            // 
+            this.btn_addText.Location = new System.Drawing.Point(43, 183);
+            this.btn_addText.Margin = new System.Windows.Forms.Padding(4);
+            this.btn_addText.Name = "btn_addText";
+            this.btn_addText.Size = new System.Drawing.Size(152, 28);
+            this.btn_addText.TabIndex = 5;
+            this.btn_addText.Text = "Dodaj tekst";
+            this.btn_addText.UseVisualStyleBackColor = true;
+            this.btn_addText.Click += new System.EventHandler(this.btn_addText_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(43, 219);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(151, 22);
+            this.textBox1.TabIndex = 6;
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.Ksztalty);
+            this.panel1.Location = new System.Drawing.Point(1135, 95);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(200, 37);
+            this.panel1.TabIndex = 7;
+            // 
+            // button8
+            // 
+            this.button8.Location = new System.Drawing.Point(0, 118);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(200, 45);
+            this.button8.TabIndex = 3;
+            this.button8.Text = "linia";
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.h);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(0, 80);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(200, 45);
+            this.button7.TabIndex = 2;
+            this.button7.Text = "kwadrat";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(0, 37);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(200, 45);
+            this.button6.TabIndex = 1;
+            this.button6.Text = "kolo";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Ksztalty
+            // 
+            this.Ksztalty.Location = new System.Drawing.Point(0, 0);
+            this.Ksztalty.Name = "Ksztalty";
+            this.Ksztalty.Size = new System.Drawing.Size(200, 45);
+            this.Ksztalty.TabIndex = 0;
+            this.Ksztalty.Text = "Kształty";
+            this.Ksztalty.UseVisualStyleBackColor = true;
+            this.Ksztalty.MouseLeave += new System.EventHandler(this.Ksztalty_MouseLeave);
+            this.Ksztalty.MouseHover += new System.EventHandler(this.Ksztalty_MouseHover);
+            // 
+            // timer1
+            // 
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.SystemColors.AppWorkspace;
+            this.panel2.Controls.Add(this.pictureBox5);
+            this.panel2.Controls.Add(this.pictureBox4);
+            this.panel2.Controls.Add(this.pictureBox3);
+            this.panel2.Controls.Add(this.pictureBox2);
+            this.panel2.Location = new System.Drawing.Point(1144, 653);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(248, 54);
+            this.panel2.TabIndex = 8;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.BackColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.pictureBox2.Location = new System.Drawing.Point(3, 3);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(38, 48);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.BackColor = System.Drawing.Color.Red;
+            this.pictureBox3.Location = new System.Drawing.Point(47, 3);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(38, 48);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BackColor = System.Drawing.Color.Green;
+            this.pictureBox4.Location = new System.Drawing.Point(91, 3);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(38, 48);
+            this.pictureBox4.TabIndex = 11;
+            this.pictureBox4.TabStop = false;
+            this.pictureBox4.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BackColor = System.Drawing.Color.Blue;
+            this.pictureBox5.Location = new System.Drawing.Point(135, 3);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(38, 48);
+            this.pictureBox5.TabIndex = 12;
+            this.pictureBox5.TabStop = false;
+            this.pictureBox5.Click += new System.EventHandler(this.pictureBox2_Click_1);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
+            this.ClientSize = new System.Drawing.Size(1404, 788);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.btn_addText);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Name = "Form1";
+            this.Text = "Paint 0.2";
+            this.WindowState = System.Windows.Forms.FormWindowState.Maximized;
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button btn_addText;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button Ksztalty;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox pictureBox2;
+    }
+}
+
---
+++ image.04/image/Form1.cs
@@ -0,0 +1,247 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace image
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+           
+        }
+        bool czyotwarte = false;
+        int p1 = 45;
+        
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog ofile = new OpenFileDialog();
+            ofile.Filter = "Image File (*.bmp,*.jpg)|*.bmp;,*.jpg;";
+            if (DialogResult.OK == ofile.ShowDialog())
+            {
+                this.pictureBox1.Image = new Bitmap(ofile.FileName);
+                czyotwarte = true;
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+            }
+            
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap(this.pictureBox1.Image) ;
+            processing.ZamienNaSzare(copy);
+            this.pictureBox1.Image = copy;
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Bitmap copy = new Bitmap(this.pictureBox1.Image);
+            processing.ZamienNaSepie(copy);
+            this.pictureBox1.Image = copy;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            if(czyotwarte==true)
+            {
+                SaveFileDialog sfd = new SaveFileDialog();
+                sfd.Filter = "Image File |*.bmp;,*.jpg;,*.png";
+                ImageFormat format = ImageFormat.Png;//zapisuje normlanie
+
+                if (sfd.ShowDialog()==System.Windows.Forms.DialogResult.OK)
+                {
+            
+                    string ext = Path.GetExtension(sfd.FileName);
+                    
+                    switch(ext)
+                    {
+                        case ".jpg":
+                            format = ImageFormat.Jpeg;
+                            break;
+                        case ".bmp":
+                            format = ImageFormat.Bmp;
+                            break;
+
+                    }
+                    pictureBox1.Image.Save(sfd.FileName, format);
+                }
+                
+
+            }
+            else
+                {
+                MessageBox.Show("Nie otworzono zdjęcia");
+            }
+        }
+
+        private void btn_addText_Click(object sender, EventArgs e)
+        {
+            CustomLabel lbl = new CustomLabel();
+            this.Controls.Add(lbl);
+
+            lbl.Top = 300;
+            lbl.Left = 300;
+            lbl.ForeColor = Color.Red;
+            lbl.BackColor = Color.Transparent;
+            lbl.Font = new Font("Arial", 20.25F, FontStyle.Regular, GraphicsUnit.Point, ((Byte)(0)));
+            lbl.AutoSize = true;
+            lbl.Text = textBox1.Text;
+            lbl.BringToFront();
+            /*
+            var image = new Bitmap(this.pictureBox1.Image);
+            var graphics = Graphics.FromImage(image);
+            graphics.DrawString(textBox1.Text, lbl.Font, Brushes.Red, new Point(lbl.Top, lbl.Left));
+            this.pictureBox1.Image = image;
+            lbl.Visible = false; 
+            */
+        }
+
+        private void Ksztalty_MouseHover(object sender, EventArgs e)
+        {
+
+            this.panel1.Size = new Size(this.panel1.Size.Width, p1);
+            timer1.Start();
+        }
+
+        private void Ksztalty_MouseLeave(object sender, EventArgs e)
+        {
+            timer1.Stop();
+            p1 = 45;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (p1 > 225)
+            { timer1.Stop(); }
+            else
+            {
+                this.panel1.Size = new Size(this.panel1.Size.Width, p1);
+                p1 += 35;
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+           // Graphics objekt = pictureBox1.CreateGraphics();
+          //  Brush red = new SolidBrush(Color.Red);
+           // Pen redPen = new Pen(red, 8);
+           // objekt.DrawLine(redPen, 10, 10, 400, 376);
+           
+        }
+
+        private void button6_Click(object sender, EventArgs e) //elipsa
+        {
+            Bitmap bm = new Bitmap(this.pictureBox1.Image);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                Brush red = new SolidBrush(Color.Red);
+            Pen redPen = new Pen(red, 8);
+            gr.DrawEllipse(redPen, 10, 10, 200, 200);
+            }
+            pictureBox1.Image = bm;
+        }
+
+        private void button8_Click(object sender, EventArgs e) //rysowanie linii
+        {
+            Bitmap bm = new Bitmap(this.pictureBox1.Image);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+                Brush red = new SolidBrush(Color.Red);
+                Pen redPen = new Pen(red, 8);
+               gr.DrawLine(redPen, 10, 10, 200, 200);
+            }
+            pictureBox1.Image = bm;
+        }
+
+        private void h(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button7_Click(object sender, EventArgs e) //rysowanie prostokata
+        {
+            // pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+            // pictureBox1.Location = new Point(0, 0);
+            Graphics objekt = pictureBox1.CreateGraphics();
+            Bitmap bm = new Bitmap(this.pictureBox1.Image);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+             
+                Brush red = new SolidBrush(Color.Red);
+                Pen redPen = new Pen(red, 8);
+                gr.DrawRectangle(redPen, 10, 10, 200, 200);
+            }
+            pictureBox1.Image = bm;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        bool czyRusza = false;
+        int rysX = -1;
+        int rysY = -1;
+        Pen rys =new Pen(Color.Black,5);
+        
+
+        private void pictureBox2_Click_1(object sender, EventArgs e)
+        {
+            
+            PictureBox  p = (PictureBox)sender;
+            rys.Color = p.BackColor;
+        }
+      
+        
+
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)//do rysowania
+        {
+            pictureBox1.Cursor = Cursors.Cross;
+            czyRusza = true;
+            rysX = e.X;
+            rysY = e.Y;
+
+        }
+
+        private void pictureBox1_MouseMove(object sender, MouseEventArgs e)//do rysowania
+        {
+           // pictureBox1.Image = DrawArea;
+           // Graphics g = pictureBox1.CreateGraphics();
+            Bitmap bm = new Bitmap(this.pictureBox1.Image);
+            using (Graphics gr = Graphics.FromImage(bm))
+            {
+
+                gr.SmoothingMode = System.Drawing.Drawing2D.SmoothingMode.AntiAlias;
+                PictureBox p = (PictureBox)sender;
+                rys.StartCap = rys.EndCap = System.Drawing.Drawing2D.LineCap.Round;
+                if (czyRusza && rysX != -1 && rysY != -1)
+                {
+                    gr.DrawLine(rys, new Point(rysX, rysY), e.Location);
+                    rysX = e.X;
+                    rysY = e.Y;
+
+                }
+            }
+            pictureBox1.Image = bm;
+        }
+
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e) //do rysowania
+        {
+            czyRusza = true;
+            rysX = -1;
+            rysY = -1;
+            pictureBox1.Cursor = Cursors.Default;
+        }
+    }
+}
